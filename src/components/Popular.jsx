7c74b1246865c40b00bac335cca848736f8493e7
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2023-03-28 16:58:31 -0400

    added api and local storage

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,71 @@
+import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import Wrapper from "./Wrapper";
+import Card from "./Card";
+import Gradient from "./Gradient";
+
 function Popular() {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+
+    const check = localStorage.getItem("popular");
+
+    if(check) {
+        setPopular(JSON.parse(check));
+
+    } else{
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+          );
+          const data = await api.json();
+
+          localStorage.setItem("popular", JSON.stringify(data.recipes));
+          setPopular(data.recipes);
+          console.log(data.recipes);
+    }
+
+    
+  };
+
   return (
-    <div>Popular</div>
-  )
+    <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+
+        <Splide options={{
+            perPage: 4,
+            arrow: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+        }}>
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 }
 
-export default Popular
+
+
+
+
+
+
+export default Popular;
