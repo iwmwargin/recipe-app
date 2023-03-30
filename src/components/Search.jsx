8496b8435154ea124335038498d9223460bb6a48
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2023-03-30 12:54:04 -0400

    added functionality to grab different cuisines also styling

---
+++ src/components/Search.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+
+
+
+function Search() {
+  return (
+    <form>
+        <input type="text" />
+    </form>
+  )
+}
+
+const FormStyle = styled.form`
+    margin: 0rem 20rem;
+    position: relative;
+    width: 100%;
+    
+`
+
+export default Search
